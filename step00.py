26c1560d384f4f5e9a362c8f3ada9e4ccc8cf6a0
Muhammad-Tayyab <m.tayyab1263@gmail.com>
2024-10-24 14:25:05 +0500

    python Journey

---
+++ step00.py
@@ -0,0 +1,22 @@
+print("Hello World");
+
+# variables and data types
+
+x: str = "zain";
+
+y: int = 1234;
+
+z: bool = True;
+
+# print(y);
+# print(x);
+# print(z);
+
+
+
+name = input("What is your name? ");
+age = input("What is your age? ");
+
+
+print("Hello, " + name);
+
